cacee3dd3df2fb01b0a7de08b3c2e0dec21d729c
Mehrzad Bazhdanzadeh <mehrzadb@outlook.com>
2017-06-20 19:26:38 -0400

    Updated folders

--- main.c
+++ main.c
@@ -9,12 +9,14 @@ void apps();
 void about();
 void aboutThisApp();
 
+char osArc[6] = {'a','m','d','6','4'};
 void main(){
 
 	char command[50];
 	char input[7] ={0};
 
 	aboutThisApp();
+	update();
 	while(1){
 		printf("<><><><><><><><><><><><><>\n");
 		printf("Input command [install/update/apps/about/exit]: ");
@@ -22,6 +24,7 @@ void main(){
 
 		if (!strcmp(input,"install")){
 			printf("INSTALL\n");
+			install();
 		}
 		else if (!strcmp(input,"update")){
 			printf("UPDATE\n");
@@ -54,11 +57,33 @@ void aboutThisApp(){
 
 void update(){
 	printf("<><><><><><><><><><><><><>\n");
-	system("sudo apt-get update");
-	system("sudo apt-get upgrade");
+	system("sudo apt-get -qq update");
+	system("sudo apt-get -qq upgrade");
+	system("mkdir ~/Programs/");
 }
 
 void apps(){
 	printf("<><><><><><><><><><><><><>\n");
 	system("ls repository/ -1");
+}
+
+void install(){
+	printf("<><><><><><><><><><><><><>\n");
+	char input[10];
+	printf("Please state the application name [***|apps]: ");
+	scanf("%s", input);
+	char cmd[60];
+	if (!strcmp(input,"apps")){
+		printf("APPS\n");
+		apps();
+	}
+	else{
+		sprintf(cmd,"mkdir ~/Programs/%s", input);
+		system(cmd);
+		sprintf(cmd,"sudo dpkg -iG repository/%s/install%s.deb --instdir=<~/Programs/%s/>", input, osArc, input);
+		printf("Requested to install %s\n", input);
+		printf("command %s\n", cmd);
+		system(cmd);
+
+	}
 }
